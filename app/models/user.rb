8a8c826b836af7b3ebe0dc2859aeac838645ab15
Shaneil Clarke <shaneil.clarke@cutover.com>
2023-08-02 13:39:23 +0100

    allowable registerstion with a username, email address and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+  
+  validates :username, presence: true, uniqueness: true
          
   def jwt_payload
     super
